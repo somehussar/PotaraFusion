1eb75d54be569175794e22262023e4ced37081fb
somehussar <148253652+somehussar@users.noreply.github.com>
2023-12-24 12:56:37 +0100

    Initial Commit

---
+++ src/main/java/io/github/somehussar/potara/PotaraMain.java
@@ -0,0 +1,10 @@
+package io.github.somehussar.potara;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = PotaraMain.MODID, version="1.0")
+public class PotaraMain {
+    @Mod.Instance(PotaraMain.MODID)
+    public static PotaraMain instance;
+    public static final String MODID = "potara";
+}
