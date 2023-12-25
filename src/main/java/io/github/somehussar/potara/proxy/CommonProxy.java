94765fc03e5a45774889c4ed748dd4627e14b73c
somehussar <148253652+somehussar@users.noreply.github.com>
2023-12-25 04:53:13 +0100

    Added basic Potara item logic

--- src/main/java/io/github/somehussar/potara/proxy/CommonProxy.java
+++ src/main/java/io/github/somehussar/potara/proxy/CommonProxy.java
@@ -1,17 +1,22 @@
 package io.github.somehussar.potara.proxy;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.*;
 import io.github.somehussar.potara.PotaraMain;
-import io.github.somehussar.potara.command.TestCommand;
+import io.github.somehussar.potara.command.GivePotara;
+import io.github.somehussar.potara.event.PlayerListener;
+import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy {
 
     public void onPreInit(FMLPreInitializationEvent e){
-
     }
 
     public void onInit(FMLInitializationEvent e){
         PotaraMain.getLogger().info("Initializing!");
+
+        MinecraftForge.EVENT_BUS.register(new PlayerListener());
+        FMLCommonHandler.instance().bus().register(new PlayerListener());
     }
 
     public void onPostInit(FMLPostInitializationEvent e) {
@@ -23,7 +28,7 @@ public class CommonProxy {
     }
 
     public void onStarting(FMLServerStartingEvent e) {
-        e.registerServerCommand(new TestCommand());
+        e.registerServerCommand(new GivePotara());
     }
 
     public void onStarted(FMLServerStartedEvent e) {
