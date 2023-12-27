343d82f40d0871aa2f04b43d54e31d4ff7ea9351
somehussar <148253652+somehussar@users.noreply.github.com>
2023-12-28 00:31:31 +0100

    Moved over entity registration to common proxy

--- src/main/java/io/github/somehussar/potara/proxy/CommonProxy.java
+++ src/main/java/io/github/somehussar/potara/proxy/CommonProxy.java
@@ -1,31 +1,38 @@
 package io.github.somehussar.potara.proxy;
 
 import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import io.github.somehussar.potara.PotaraConfig;
 import io.github.somehussar.potara.PotaraMain;
 import io.github.somehussar.potara.command.GivePotara;
-import io.github.somehussar.potara.entity.CustomEntityRegistry;
 import io.github.somehussar.potara.entity.EntityThrownPotara;
 import io.github.somehussar.potara.event.PlayerListener;
 import io.github.somehussar.potara.item.PotaraItem;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy {
 
     public void onPreInit(FMLPreInitializationEvent e){
         PotaraConfig.init(e.getModConfigurationDirectory().getAbsolutePath());
-        CustomEntityRegistry.registerEntity(EntityThrownPotara.class, "ThrownPotara");
+        registerEntity(EntityThrownPotara.class, "ThrownPotara");
         PotaraItem.init();
     }
 
     public void onInit(FMLInitializationEvent e){
         PotaraMain.getLogger().info("Initializing!");
-
         MinecraftForge.EVENT_BUS.register(new PlayerListener());
     }
 
     public void onStarting(FMLServerStartingEvent e) {
         e.registerServerCommand(new GivePotara());
+    }
+
+
+    public static void registerEntity(Class<? extends Entity> clazz, String name){
+        PotaraMain.getLogger().info(String.format("Registering entity `%s`, class: %s", name, clazz.getName()));
+        int randomInt = EntityRegistry.findGlobalUniqueEntityId();
+        EntityRegistry.registerGlobalEntityID(clazz, name, randomInt);
     }
 
 }
